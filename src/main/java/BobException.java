2905288fc87325ce6febd8483ed8ec95d929245e
john <john.woo.yk@gmail.com>
2025-02-08 21:35:00 +0800

    Create a BobException class for custom errors

---
+++ src/main/java/BobException.java
@@ -0,0 +1,18 @@
+/**
+ * Custom exception class for exceptions specific to Bob.
+ * The BobException class extends the built-in Exception class
+ * and provides error message constructors for errors
+ * specific to Bob only.
+ */
+
+public class BobException extends Exception {
+
+    /**
+     * Constructs a BobException with custom error messages
+     *
+     * @param message String with the custom message
+     */
+    public BobException(String message) {
+        super(message);
+    }
+}
