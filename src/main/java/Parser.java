14f85808e1322a31b5f194fe99aeed048dd82d1a
John <john.woo.yk@gmail.com>
2025-02-12 00:47:13 +0800

    Fix logical error check for Events, and added tests for error checking

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -111,15 +111,21 @@ public class Parser {
                 throw new BobException("Okay we are checking... there's no description." +
                         "Please add a description."); //Missing space before description
             }
-            String[] eventArray = input.substring(taskPosition).split("/from ", 2); //Split the description and deadline
-            String eventName = eventArray[0].trim();
-            String[] fromToArray = eventArray[1].split(" /to", 2);
 
-            if (eventArray.length != 2 || fromToArray.length != 2) {
-               throw new BobException("Okay we are checking... there's no from/to." +
-                       "Please add /from and /to to demarcate the duration of the event.");
+            String[] eventArray = input.substring(taskPosition).split("/from ", 2); //Split the description and event
+            if (eventArray.length != 2) {
+                throw new BobException("Okay we are checking... there's no from." +
+                        "Please add /from to demarcate the duration of the event.");
             }
 
+            String[] fromToArray = eventArray[1].split(" /to", 2);
+            //This error check needs to be split with the /from check, or you will get index out of bounds
+            if (fromToArray.length != 2) {
+                throw new BobException("Okay we are checking... there's no to." +
+                        "Please add /to to demarcate the duration of the event.");
+            }
+
+            String eventName = eventArray[0].trim();
             String startDate = fromToArray[0].trim();
             String endDate = fromToArray[1].trim();
             taskList.add(new Event(eventName, startDate, endDate));
